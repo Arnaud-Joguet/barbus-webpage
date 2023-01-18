c2240bf91b28d0a38138d1e8c15f36f9c62e9958
Arnaud <arnaud.joguet@gmail.com>
2023-01-18 10:17:40 +0100

    add post's title

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -32,6 +32,9 @@ class Post
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $updatedAt = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -105,6 +108,18 @@ class Post
     public function setUpdatedAt(?\DateTimeImmutable $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
 
         return $this;
     }
