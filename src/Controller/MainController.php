3be089e10c2a68cc6ac1c11052dac7d8d3534f9c
Arnaud <arnaud.joguet@gmail.com>
2023-01-19 14:45:34 +0100

    add last post card

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping\PostPersist;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+        $lastPost = $postRepository->getLastpost();
+        //dump($lastPost);
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'lastPost' => $lastPost,
         ]);
     }
 }
