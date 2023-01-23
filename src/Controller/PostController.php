9a88a599061b640ce7a9d09939d1ba24ec6a0547
Arnaud <arnaud.joguet@gmail.com>
2023-01-23 10:43:28 +0100

    code(PostController): show method

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,11 +14,24 @@ class PostController extends AbstractController
     public function list(PostRepository $postRepository): Response
     {
 
-
         $postList = $postRepository->findAll();
 
         return $this->render('post/list.html.twig', [
             'postList' => $postList
+        ]);
+
+    }
+
+
+    #[Route('/post/{id}',name: 'post_show', requirements: ['id' => '\d+'])]
+    public function show(Post $post = null ): Response
+    {
+        if(is_null($post)) {
+            throw $this->createNotFoundException('The post does not exist');
+        }
+
+        return $this->render('post/show.html.twig', [
+            'post' => $post
         ]);
 
     }
