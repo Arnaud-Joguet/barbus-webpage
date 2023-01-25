4a04aaf065319ded30267b0972929803548c405a
Arnaud <arnaud.joguet@gmail.com>
2023-01-25 09:48:12 +0100

    create userController

---
+++ src/Controller/Back/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Back;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/back/user')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'app_back_user')]
+    public function index(): Response
+    {
+        return $this->render('back/user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
